7605e63fff93b71ba569245e12cac6afda8ce41f
phoenixwilliams <32957699+phoenixwilliams@users.noreply.github.com>
2021-10-11 13:59:08 +0100

    update

--- Deployable/PWv1.py
+++ Deployable/PWv1.py
@@ -15,19 +15,24 @@ from AcquisitionOptimizers.lbfgsb import AOlbfgs
 
 
 class BO:
-    def __init__(self, function, iterations, lb, ub, dim, x_init=None, y_init=None, n_init=10.):
+    def __init__(self, function, iterations, lb, ub, x_init=None, y_init=None, n_init: int = 10):
 
         self.function = function
         self.iterations = iterations
         self.lb = lb
         self.ub = ub
-        self.dim = dim
-
-        assert len(x_init) == len(y_init) or x_init == y_init
+        assert len(lb) == len(ub)
+        assert type(n_init) == int
+        self.dim = len(lb)
 
         if x_init is None:
-            self.x_init = lhs(dim, n_init) * (self.ub - self.lb) + self.lb
+            assert y_init is None
+            self.x_init = lhs(self.dim, n_init) * (self.ub - self.lb) + self.lb
             self.y_init = np.array([self.function(xi) for xi in self.x_init]).reshape(-1, 1)
+
+        else:
+            self.x_init = x_init
+            self.y_init = y_init
 
     def opt(self, verbose=None):
 
@@ -44,21 +49,24 @@ class BO:
                 print("iteration %i min value: %.5f" % (it, min(y)))
 
             # 1. Design and Train Model
-            kernel = RBF(x.shape, ARD=True)
+            kernel = RBF(x.shape, ARD=False)
             model = GPregression(kernel, x, y)
             likelihood = Likelihood(model)
             # call likelihood.evaluate() -> if you do not wish to train the model
             model_optimizer = lbfgsb(model)
             model_optimizer.opt()
+            print(model.log_likelihood)
 
             # 2. Select next sample point
-            acquisition = AcquisitionEI(model_optimizer, min(y))
+            acquisition = AcquisitionEI(model, min(y))
             acquisition_optimizer = AOlbfgs(acquisition, self.lb, self.ub)
-            sample, ei_val = acquisition_optimizer.opt(restarts=5)
+            sample, ei_val = acquisition_optimizer.opt(restarts=10)
+
 
             # 3. Evaluate Sample and append to the dataset
             s_eval = self.function(sample)
-            x = np.concatenate([x, sample])
-            y = np.concatenate([y, s_eval], axis=0)
+            x = np.concatenate([x, np.array([sample])], axis=0)
+            y = np.concatenate([y, np.array([[s_eval]])], axis=0)
 
+        print("final min value:", min(y))
         return x, y
